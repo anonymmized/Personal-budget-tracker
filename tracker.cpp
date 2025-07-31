3f53ed92d5fc24d5b593d58265fbc8e78d9aea2f
anonymmized <lih0deev@inbox.ru>
2025-07-31 19:10:53 +0300

    ViewTransaction

--- tracker.cpp
+++ tracker.cpp
@@ -9,9 +9,44 @@ struct Transaction {
     double amount;
 };
 
-//void newTransaction(vector(Transaction)& transactions) {
+void newTransactions(vector<Transaction>& transactions) {
+    string transactionDescription, amountInput;
+    double transactionAmount;
+    cout << "\033[32mEnter transaction description: \033[0m";
+    getline(cin, transactionDescription);
+    if (transactionDescription.empty()) {
+        cout << "\033[32mTransaction description cannot be empty!\033[0m" << endl;
+        return;
+    }
+
+    cout << "\033[32mEnter transaction amount: \033[0m";
+    getline(cin, amountInput);
+    try {
+        transactionAmount = stod(amountInput);
+    } catch(...) {
+        cout << "\033[32mInvalid amount!\033[0m" << endl;
+        return;
+    }
     
-//}
+    Transaction newTransaction;
+    newTransaction.description = transactionDescription;
+    newTransaction.amount = transactionAmount;
+    transactions.push_back(newTransaction);
+    cout << "\033[32mTransaction added: " << transactionDescription <<  " - Amount: " << transactionAmount << "$\033[0m" << endl;
+
+}
+
+void viewTransactions(const vector<Transaction>& transactions) {
+    if (transactions.empty()) {
+        cout << "\033[32mNo transactions!\033[0m" << endl;
+        return;
+    }
+
+    cout << "\033[32m--- Transactions list ---\033[0m" << endl;
+    for (size_t i = 0; i < transactions.size(); i++) {
+        cout << i + 1 << "\033[32m. " << transactions[i].description << " - " << transactions[i].amount << "$\033[0m" << endl;
+    }
+}
 
 int main() {
     vector<Transaction> transactions;
@@ -20,8 +55,9 @@ int main() {
     while (true) {
         cout << "\033[31m--- Menu ---\033[0m" << endl;
         cout << "\033[31m1. Add transaction\033[0m" << endl;
-        cout << "\033[31m2. Exit\033[0m" << endl;
-        cout << "\033[35mEnter your choice (1-2): \033[0m";
+        cout << "\033[31m2. View transactions\033[0m" << endl;
+        cout << "\033[31m3. Exit\033[0m" << endl;
+        cout << "\033[35mEnter your choice (1-3): \033[0m";
         int choice;
         if (!(cin >> choice)) {
             cout << "\033[33mInvalid input! Enter a number.\033[0m" << endl;
@@ -31,9 +67,10 @@ int main() {
         }
         cin.ignore(10000, '\n');
         if (choice == 1) {
-            cout << "\033[34mFunction in proccessing\033[0m" << endl;
-            break;
+            newTransactions(transactions);
         } else if (choice == 2) {
+            viewTransactions(transactions);
+        } else if (choice == 3) {
             cout << "\033[34mThanks for using Personal Budget Tracker!\033[0m" << endl;
             break;
         }
