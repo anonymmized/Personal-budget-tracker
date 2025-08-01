133bb4fbfc0e994c4d79b3098d551e6dde0c9c96
anonymmized <lih0deev@inbox.ru>
2025-08-01 17:31:14 +0300

    CalculateBalance

--- tracker.cpp
+++ tracker.cpp
@@ -48,6 +48,22 @@ void viewTransactions(const vector<Transaction>& transactions) {
     }
 }
 
+void calculateBalance(const vector<Transaction>& transactions) {
+    double currentBalance;
+
+    if (transactions.empty()) {
+        cout << "\033[32mNo transactions!\033[0m" << endl;
+        return;
+    }
+
+    
+    for (size_t i = 0; i < transactions.size(); i++){ 
+        currentBalance = currentBalance + transactions[i].amount;
+    }
+    cout << "\033[32mCurrent Balance: " << currentBalance << "\033[0m" << endl;
+
+}
+
 int main() {
     vector<Transaction> transactions;
     cout << "\033[31mWelcome to Personal Budget Tracker!\033[0m" << endl;
@@ -56,7 +72,8 @@ int main() {
         cout << "\033[31m--- Menu ---\033[0m" << endl;
         cout << "\033[31m1. Add transaction\033[0m" << endl;
         cout << "\033[31m2. View transactions\033[0m" << endl;
-        cout << "\033[31m3. Exit\033[0m" << endl;
+        cout << "\033[31m3. View current balance\033[0m" << endl;
+        cout << "\033[31m4. Exit\033[0m" << endl;
         cout << "\033[35mEnter your choice (1-3): \033[0m";
         int choice;
         if (!(cin >> choice)) {
@@ -71,6 +88,8 @@ int main() {
         } else if (choice == 2) {
             viewTransactions(transactions);
         } else if (choice == 3) {
+            calculateBalance(transactions);
+        } else if (choice == 4) {
             cout << "\033[34mThanks for using Personal Budget Tracker!\033[0m" << endl;
             break;
         }
